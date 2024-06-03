7ca9795137843e9f5fe514a1b6185ce5efee77ce
Maria Gruber <121759259+MariaGruber@users.noreply.github.com>
2024-06-03 10:42:24 +0200

    moviedeatils page added/ cors conflics solved

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,4 +1,26 @@
+import { getMovieById } from "@/lib/data"
+import Link from "next/link"
 
-export default async function MovieDetails(){
-return <p>I'm not ready yet, but I will be soon. Come back anytime!</p>
+type Props = {
+    params: {
+      id: string
+    }
+}
+
+export default async function MovieDetails({ params: { id }}:Props){
+    const movie = await getMovieById(id)
+    console.log(movie)
+return (
+    <div className="pl-8">
+        <img className="w-2/4" src={movie.image} alt="poster" />
+        <div>
+            <h1 className="text-2xl font-bold mt-2">{movie.name}</h1>
+            <p className="text-2xl font-bold mt-2">{movie.score}/10</p>
+            <p className="text-lg mt-2 pr-4" >{movie.synopsis}</p>
+        </div>
+        <div className="hover:cursor-pointer bg-yellow-500 mt-8 p-2 text-lg text-black font-bold rounded-lg text-center mx-32">
+        <Link href="/movies">Go back</Link>
+        </div>
+    </div>
+)
 }
--- src/services/api/api.tsx
+++ src/services/api/api.tsx
@@ -4,3 +4,8 @@ export const fetchMovies = async () => {
     const response = await axios.get('http://localhost:4001/movies');
     return response.data;
   }
+
+  export async function getMovieById(id: string) {
+    const response = await axios.get(`http://localhost:4001/movies/${id}`);
+    return response.data;
+  }
